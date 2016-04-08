58d876261adda9c2a51a87d3cdb78fb0a65e3008
Izad Che Muda <izad@selklik.com>
2016-04-08 11:42:52 +0800

    Initial Commit

---
+++ Perutusan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Perutusan
+//
+//  Created by Izad Che Muda on 4/8/16.
+//  Copyright © 2016 Izad Che Muda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
