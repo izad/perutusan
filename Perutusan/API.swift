0f52b9832b0f591b3d34cee9d7a838049a194e89
Izad Che Muda <izad@selklik.com>
2016-04-08 11:57:21 +0800

    Initial commit

---
+++ Perutusan/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  Perutusan
+//
+//  Created by Izad Che Muda on 4/8/16.
+//  Copyright © 2016 Izad Che Muda. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import EVReflection
+
+
+
---
+++ Perutusan/LoadingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingViewController.swift
+//  Perutusan
+//
+//  Created by Izad Che Muda on 4/8/16.
+//  Copyright © 2016 Izad Che Muda. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
